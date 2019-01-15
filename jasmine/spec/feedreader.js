2037676a4e12240cb8203680e67c14326bf4f6c2
meijk <michael.schick@posteo.de>
2019-01-16 09:09:05 +1000

    Apply ES6 style to feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -2,24 +2,24 @@ $(function() {
 
     /* Suite to test if all RSS feeds in the allFeeds array have
     a defined name and destination */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         // Checks if allFeeds array is defined and not empty
-        it('are defined', function() {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
         // Loops through all feeds and checks for defined URL
-        it('have defined URL', function() {
-            allFeeds.forEach(function(feed) {
+        it('have defined URL', () => {
+            allFeeds.forEach((feed) => {
                 expect(feed.url).toBeDefined();
                 expect(feed.url).not.toBe('');
             })
         });
 
         // Tests if all feeds have a defined name
-        it('have defined name', function() {
-            allFeeds.forEach(function(feed) {
+        it('have defined name', () => {
+            allFeeds.forEach((feed) => {
                 expect(feed.name).toBeDefined();
                 expect(feed.name).not.toBe('');
             })
@@ -28,15 +28,15 @@ $(function() {
 
 
     /* Test suite to check main menu functionality */
-    describe('The menu', function() {
+    describe('The menu', () => {
         var body = document.querySelector('body');
 
         // Tests DOM event to make sure main menu is hidden by default
-        it('is hidden by default', function() {
+        it('is hidden by default', () => {
             expect(body.classList).toContain('menu-hidden');
         });
         // Sets click triggers to simulate user interaction & checks functionality
-        it ('shows and hides on menu icon click', function() {
+        it ('shows and hides on menu icon click', () => {
             $('.menu-icon-link').trigger('click');
             expect(body.classList).not.toContain('menu-hidden');
 
@@ -46,39 +46,39 @@ $(function() {
     });
 
     /* Test suite to examine initial entries */
-    describe('Initial Entries', function() {
+    describe('Initial Entries', () => {
         // Load first feed in allFeeds array
-        beforeEach(function(done) {
-            loadFeed(0, function() {
+        beforeEach((done) => {
+            loadFeed(0, () => {
                 done();
             });
         });
         // Checks for the presence of .entry elements, i.e. feed entries have been loaded
-        it('are present', function(done) {
+        it('are present', (done) => {
             expect(document.querySelectorAll('.feed .entry').length).not.toBe(0);
             done();
         });
     });
 
     /* Test suite to make sure new feeds load upon selection via main menu */
-    describe('New Feed Selection', function() {
+    describe('New Feed Selection', () => {
 
         var entriesFeed1,
             entriesFeed2,
             feed = document.querySelector('.feed');
 
         // Load 2 different feeds and store their content
-        beforeEach(function(done) {
-            loadFeed(0, function() {
+        beforeEach((done) => {
+            loadFeed(0, () => {
                 entriesFeed1 = feed.innerText;
-                loadFeed(1, function() {
+                loadFeed(1, () => {
                     entriesFeed2 = feed.innerText;
                     done();
                 });
             });
         });
         // Compare variables to assure reader successfully loads Feed2 entries
-        it('gets loaded', function(done) {
+        it('gets loaded', (done) => {
             expect(entriesFeed1).not.toBe(entriesFeed2);
             done();
         });
